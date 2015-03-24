64252dbc0c690bc4d3dde6997d11c709a0c9ae6f
Víctor Mas <victor.mas@est.fib.upc.edu>
2015-03-24 15:49:34 +0100

    Added buffering on trigon.c

--- trigon/trigon.c
+++ trigon/trigon.c
@@ -1,4 +1,3 @@
-
 #include <math.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,11 +6,15 @@
 
 #define N 6000
 #define PUNTS 1000
+#define BUFFER_SIZE 5096
 
 int main(int argc, char *argv[])
 {
 	unsigned int i, r, j, n;
 	double d, x, y;
+
+	double buffer[BUFFER_SIZE];
+	int index = 0;
 
 	if (argc == 1) n = N; else n = atoi(argv[1]);
 
@@ -22,10 +25,17 @@ int main(int argc, char *argv[])
 		for (j=0, d=0; j<PUNTS; j++)
 		{
 			x = r*cos(d); y = r*sin(d);
-			write(1, &x, sizeof(x));
-			write(1, &y, sizeof(y));
+			// write(1, &x, sizeof(double));
+			// write(1, &y, sizeof(double));
+			buffer[index++] = x;
+			buffer[index++] = y;
+			if (index >= BUFFER_SIZE) {
+				write(1, buffer, BUFFER_SIZE*sizeof(double));
+				index = 0;
+			}
 			d += 2*M_PI/PUNTS;
 		}
 	}
+	write(1, buffer, index*sizeof(double));
 	return 0;
 }
